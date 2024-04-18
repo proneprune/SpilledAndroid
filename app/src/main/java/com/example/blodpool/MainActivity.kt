2c7c8d59958ecbbf5ade1b37b6728eb5d184b29a
proneprune <jcullgert@hotmail.se>
2024-04-18 16:47:35 +0200

    removed unused functions

--- app/src/main/java/com/example/blodpool/MainActivity.kt
+++ app/src/main/java/com/example/blodpool/MainActivity.kt
@@ -105,19 +105,5 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BlodpoolTheme {
-        Greeting("Android")
-    }
-}
 
--- opencv/build/intermediates/cxx/Debug/2f6t2w56/logs/x86_64/build_command_opencv_jni_shared.bat
+++ opencv/build/intermediates/cxx/Debug/2f6t2w56/logs/x86_64/build_command_opencv_jni_shared.bat
@@ -1,5 +1,5 @@
 @echo off
-"C:\\Users\\Admin\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+"C:\\Users\\jcull\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
   -C ^
-  "C:\\Users\\Admin\\Desktop\\projekt\\blappAndroid\\opencv\\.cxx\\Debug\\2f6t2w56\\x86_64" ^
+  "C:\\Users\\jcull\\OneDrive\\Skrivbord\\Kurser\\II1305\\blapp\\blappAndroid\\opencv\\.cxx\\Debug\\2f6t2w56\\x86_64" ^
   opencv_jni_shared
