43ec5e6ccb78c9ded8f2ef84a6552a2b21896ee1
GustavB15 <118982756+GustavB15@users.noreply.github.com>
2024-04-23 15:31:15 +0200

    dssasadsdsada

--- opencv/build/intermediates/cxx/Debug/2f6t2w56/logs/x86/configure_command.bat
+++ opencv/build/intermediates/cxx/Debug/2f6t2w56/logs/x86/configure_command.bat
@@ -1,6 +1,6 @@
 @echo off
 "C:\\Users\\Admin\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\cmake.exe" ^
-  "-HC:\\Users\\Admin\\Desktop\\projectWitharcore\\main\\opencv\\libcxx_helper" ^
+  "-HC:\\Users\\Admin\\Desktop\\projCpp\\blappAndroid\\opencv\\libcxx_helper" ^
   "-DCMAKE_SYSTEM_NAME=Android" ^
   "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
   "-DCMAKE_SYSTEM_VERSION=21" ^
@@ -11,9 +11,9 @@
   "-DCMAKE_ANDROID_NDK=C:\\Users\\Admin\\AppData\\Local\\Android\\Sdk\\ndk\\25.1.8937393" ^
   "-DCMAKE_TOOLCHAIN_FILE=C:\\Users\\Admin\\AppData\\Local\\Android\\Sdk\\ndk\\25.1.8937393\\build\\cmake\\android.toolchain.cmake" ^
   "-DCMAKE_MAKE_PROGRAM=C:\\Users\\Admin\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\Users\\Admin\\Desktop\\projectWitharcore\\main\\opencv\\build\\intermediates\\cxx\\Debug\\2f6t2w56\\obj\\x86" ^
-  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\Users\\Admin\\Desktop\\projectWitharcore\\main\\opencv\\build\\intermediates\\cxx\\Debug\\2f6t2w56\\obj\\x86" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\Users\\Admin\\Desktop\\projCpp\\blappAndroid\\opencv\\build\\intermediates\\cxx\\Debug\\2f6t2w56\\obj\\x86" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\Users\\Admin\\Desktop\\projCpp\\blappAndroid\\opencv\\build\\intermediates\\cxx\\Debug\\2f6t2w56\\obj\\x86" ^
   "-DCMAKE_BUILD_TYPE=Debug" ^
-  "-BC:\\Users\\Admin\\Desktop\\projectWitharcore\\main\\opencv\\.cxx\\Debug\\2f6t2w56\\x86" ^
+  "-BC:\\Users\\Admin\\Desktop\\projCpp\\blappAndroid\\opencv\\.cxx\\Debug\\2f6t2w56\\x86" ^
   -GNinja ^
   "-DANDROID_STL=c++_shared"
