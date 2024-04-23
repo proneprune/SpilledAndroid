bc57f298c64c151df0bb63f2b47df970a1cfc902
GustavB15 <118982756+GustavB15@users.noreply.github.com>
2024-04-23 10:41:24 +0200

    Delete opencv directory

--- opencv/build/intermediates/cxx/Debug/2f6t2w56/logs/arm64-v8a/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\Admin\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\Admin\\Desktop\\projCpp\\blappAndroid\\opencv\\.cxx\\Debug\\2f6t2w56\\arm64-v8a" ^
-  opencv_jni_shared
